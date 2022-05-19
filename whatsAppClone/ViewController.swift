a0b0f83c0e6c628c6b65bb894e8cdc1453dca7a4
Vitor Henrique <vhenriquebm@gmail.com>
2022-05-19 13:57:11 -0300

    Initial Commit

---
+++ whatsAppClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatsAppClone
+//
+//  Created by Vitor Henrique Barreiro Marinho on 19/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
